a46a41a70d9c49fcd25f65d9712494205cc89c2e
Vincent Huynh <vincent.huynh123@gmail.com>
2022-02-11 22:44:13 -0500

    progress

--- _build/jupyter_execute/engagement.py
+++ _build/jupyter_execute/engagement.py
@@ -1,7 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+import ipywidgets as widgets
+
+
 # # Engagement
+
+# ## Bridesmaids / Groomsmen
+
+# The following is to help us calculate the costs associated with bridesmaids and groomsmen.
+
+# In[2]:
+
+
+widgets.IntSlider(
+    value=7,
+    min=0,
+    max=10,
+    step=1,
+    description='Test:',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d'
+)
+
 
 # ## Food
 # 
