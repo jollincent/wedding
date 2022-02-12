64436e277a655da0b729bf38fcecd512824d5bd4
Vincent Huynh <vincent.huynh123@gmail.com>
2022-02-11 23:00:22 -0500

    progress

--- _build/jupyter_execute/engagement.py
+++ _build/jupyter_execute/engagement.py
@@ -16,18 +16,19 @@ import ipywidgets as widgets
 # In[2]:
 
 
-widgets.IntSlider(
+numBridesmaids = widgets.IntSlider(
     value=7,
     min=0,
     max=10,
     step=1,
-    description='Test:',
+    description='Number:',
     disabled=False,
     continuous_update=False,
     orientation='horizontal',
     readout=True,
     readout_format='d'
 )
+display(numBridesmaids)
 
 
 # ## Food
@@ -45,3 +46,14 @@ widgets.IntSlider(
 
 # ## Outfits
 # 
+
+# ## Estimated Running Total
+# 
+# The following is the estimated running total. Please run the code cell below to calculate the current running total based on the sliders above.
+
+# In[3]:
+
+
+print("With {} bridesmaids and groomsmen at a price of ${} for each:".format(numBridesmaids.value, 100))
+print("${:.2f}".format(numBridesmaids.value * 100.192))
+
