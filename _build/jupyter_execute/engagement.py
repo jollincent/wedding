d0f4ef287f93dfeef970354eb0fa2e14b5c7d98a
Vincent Huynh <vincent.huynh123@gmail.com>
2022-02-11 19:29:44 -0500

    update

---
+++ _build/jupyter_execute/engagement.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Engagement
+
+# ## Food
+# 
+# Food we should have at the engagement:
+# - Roast Pig
+# - Roast Duck
+# - Dessert Foods
+
+# ## Ceremonial Events
+# ... and associated estimated prices.
+
+# ## Decorations
+# 
+
+# ## Outfits
+# 
---
+++ _build/jupyter_execute/reception.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Reception
+
+# ## Venue
+# 
+# We only know the following requirements:
+# - At least 30 tables (assuming 10 people each), up to 50 tables (30-40 for Vincent's side)
+# - Accessible location (not too far)
+# - Good parking
+# - Food is not a requirement (catering is an option)
+# 
+# And the following preferences:
+# - Rustic look
+# - Tall / good ceiling height
+
+# ## Reception Events & Things
+# 
+# The tentative reception plan:
+# 
+# [**TODO**] Insert things for reception plan
+
+# ## Decorations
+
+# ### Live Band or Karoake Machine
+# 
+# We definitely wanted some music from a live band but if not, a karoake machine sounds good.
+# 
+# > We plan to have friends and family sign up to sing first, then open up the floor to guests. Jollin wants the bride and groom to sing a song together.
+
+# ### Photo Booth
+# Photo booth with on-site printing or on-site tags for online distribution would be cool. Consider something [like this](https://shutterbooth.com/atlanta/), which is a kiosk or photo booth with different cool gif, boomerang, etc.
+# 
+# It would be cool to make it ourselves so we can save money and reuse it, but if it fits in our budget, we can rent one.
+
+# ## Invitations / Envelopes
+# Vincent's dad mentioned going to Vietnam to print envelopes; however, if a trip to Vietnam is not available, we may need to order them online.
+# 
+# [**TODO**] Look up different prices for invitation printing.
